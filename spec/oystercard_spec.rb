0efcb917e9df2e65f3f6ec4bf611cb4678041f91
rizinaa99 <rizinakhatun@icloud.com>
2022-01-04 16:23:42 +0000

    completed up to 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,17 @@
+require 'oystercard'
+
 describe Oystercard do
+  # it 'Should have an initial balance of 0' do
+  #   expect(subject.balance).to eq(0)
+  # end
+
+  describe '#top_up' do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    it "can top up the oystercard" do 
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+
+  end
+
 end
